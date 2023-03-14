9dd71e53d5331eda680044eb58a57682f38a13a1
Даниил Орлов <danilorlov9898@yandex.ru>
2023-03-15 01:12:45 +0300

    Tryied My CSS-Custom

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -7,8 +7,10 @@ SECRET_KEY = 'ovg_1ouv*yc254x&+^aa)&f7vz#xzf2kd$k!&a#0k!_-6+xv5x'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'www.daniilorlovv.pythonanywhere.com',
-    'daniilorlovv.pythonanywhere.com',
+    'localhost',
+    '127.0.0.1',
+    '[::1]',
+    'testserver',
 ]
 
 
