e4f6633755410c654c16f20e94c01fde9ddfbd98
Даниил Орлов <danilorlov9898@yandex.ru>
2023-03-18 02:26:46 +0300

    New Changes for Styles

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -100,7 +100,6 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')#Для работы стилей админки на сервере
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
